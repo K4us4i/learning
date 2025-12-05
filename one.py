158cff66f9abf66149b47f71302cfd85195a8007
Khushi Paudel Khatri <khatripaudel0@gmail.com>
2025-12-05 14:42:01 +0545

    mood tracker

--- one.py
+++ one.py
@@ -1,1 +1,27 @@
-printf("hello")
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    joke = None
+
+    if request.method == "POST":
+        # User mood input
+        mood_text = request.form.get("mood")
+
+        # Call API (simple joke API for now)
+        url = "https://v2.jokeapi.dev/joke/Any"
+        response = requests.get(url)
+        data = response.json()
+
+        if data["type"] == "single":
+            joke = data["joke"]
+        else:
+            joke = data["setup"] + " ..." + data["delivery"]
+
+    return render_template("index.html", joke=joke)
+
+if __name__ == "__main__":
+    app.run(debug=True)
